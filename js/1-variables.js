92af6ddfb74761bda16408732fc8b3d4ddb5cec6
Gerardo Mansilla <gerardoemansilla@gmail.com>
2023-03-07 13:39:20 -0300

    agregué Highlight.js al proyecto

--- js/1-variables.js
+++ js/1-variables.js
@@ -1,13 +1,3 @@
-//asd
-/*
-let nombreUsuario = "Gerardo Mansilla";
-let anio = 2023
-
-
-console.log(`${nombreUsuario} es alumno del año ${anio}`);
-document.write(nombreUsuario + " es alumno del año " + anio);
-*/
-
 function ejercicio1(){
     //main.innerHTML(`alert("un mensaje")`);
     console.log("click!")
@@ -111,62 +101,7 @@ function ejercicio11(){
 }
 
 /*
-seaside summer days scoreboard/ notice board
-teenage blue scoreboard
-CNY scoreboard
 let frase = prompt();
 const vocales = /[aeiouAEIOU]/g;
 document.write(`vocales en la frase: ${frase.match(vocales)}`);
 */
-
-
-/* let numero = prompt();
-if (numero%2 == 0) {
-    document.write(`${numero} es divisible en 2`);
-} else if (numero%3 == 0) {
-        document.write(`${numero} es divisible en 3`);
-    } else if (numero%5 == 0){
-        document.write(`${numero} es divisible en 5`);
-    } else if (numero%7 == 0){
-        document.write(`${numero} es divisible en 7`);
-    } */
-
-
-/*
-let numero = parseInt(prompt("Ingrese un número"));
-console.log(numero);
-if (numero%2 === 0) {
-    document.write(`${numero} es divisible en 2<br>`);
-    console.log(`${numero} es divisible en 2<br>`)
-    if (numero%3 == 0) {
-        document.write(`${numero} es divisible en 3<br>`);
-        if (numero%5 == 0){
-            document.write(`${numero} es divisible en 5<br>`);
-            console.log(`${numero} es divisible en 5<br>`)
-            if (numero%7 == 0){
-                document.write(`${numero} es divisible en 7<br>`);
-            }
-        }
-    }
-}else{
-    document.write(`${numero} no es divisible en 2,3,5 ni 7`);
-}
-*/
-
-/* switch (numero) {
-    case numero%2 == 0:
-        document.write(`${numero} es divisible en 2`);
-        break;
-    case numero%3 == 0:
-        document.write(`${numero} es divisible en 3`);
-        break;
-    case numero%5 == 0:
-        document.write(`${numero} es divisible en 5`);
-        break;
-    case numero%7 == 0:
-        document.write(`${numero} es divisible en 7`);
-        break;
-    default:
-        document.write(`${numero} no es divisible en 2,3,5 ni 7`);
-        break;
-} */
